3ce3cd1b25d77e4360389fde56bbddedffca98f7
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-08 22:04:10 +0200

    Add initial project files

---
+++ src/main/java/com/example/zasobnik/ZasobnikApplication.java
@@ -0,0 +1,13 @@
+package com.example.zasobnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZasobnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZasobnikApplication.class, args);
+	}
+
+}
