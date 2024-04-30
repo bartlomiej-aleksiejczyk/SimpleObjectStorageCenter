7dca50e76911264e49f252eebae605c1361eda04
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-30 09:21:40 +0200

    Add findbySlub method to flatbox repository

--- backend/src/main/java/com/example/zasobnik/flatbox/FlatboxRepository.java
+++ backend/src/main/java/com/example/zasobnik/flatbox/FlatboxRepository.java
@@ -1,7 +1,11 @@
 package com.example.zasobnik.flatbox;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlatboxRepository extends JpaRepository<Flatbox, Long> {
     boolean existsBySlug(String slug);
+
+    Optional<Flatbox> findBySlug(String slug);
 }
