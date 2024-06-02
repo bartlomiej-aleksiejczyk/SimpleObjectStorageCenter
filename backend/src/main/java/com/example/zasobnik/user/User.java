3035812102a5cfcd60c6b79eb545663be04fb241
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-06-02 11:20:11 +0200

    Add custom user service

--- backend/src/main/java/com/example/zasobnik/user/User.java
+++ backend/src/main/java/com/example/zasobnik/user/User.java
@@ -1,5 +1,21 @@
 package com.example.zasobnik.user;
 
-public class User extends BaseEntity {
+import com.example.zasobnik.common.BaseEntity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User extends BaseEntity {
+    private String username;
+    private String password;
+    private String roles;
 }
