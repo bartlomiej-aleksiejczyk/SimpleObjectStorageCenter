f31793b6aa8d63c38ccd94cc8283015e17e897f8
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-06-04 20:05:35 +0200

    Add commandline initializer that add default user

---
+++ backend/src/main/java/com/example/zasobnik/user/UserInitializer.java
@@ -0,0 +1,25 @@
+package com.example.zasobnik.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+import com.example.zasobnik.user.UserService;
+
+@Component
+@RequiredArgsConstructor
+public class UserInitializer implements CommandLineRunner {
+    private final UserService userService;
+
+    @Value("${SPRING_SINGLE_LOGIN}")
+    private String login;
+
+    @Value("${SPRING_SINGLE_PASSWORD}")
+    private String rawPassword;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userService.createUser(login, rawPassword);
+    }
+}
