cbcc2ba57b429747ffec94151703ae04b58524ae
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-06-05 19:34:13 +0200

    Add docstring to user global permissions

--- backend/src/main/java/com/example/zasobnik/user/UserRole.java
+++ backend/src/main/java/com/example/zasobnik/user/UserRole.java
@@ -1,5 +1,21 @@
 package com.example.zasobnik.user;
 
 public enum UserRole {
-    ROLE_ADMINISTRATOR, ROLE_INSPECTOR, ROLE_MODERATOR, ROLE_SUPER_EDITOR
+    /**
+     * Permits the user to perform all available actions on everything.
+     */
+    ROLE_ADMINISTRATOR,
+    /**
+     * Allows the user to read every log and file without making changes.
+     */
+    ROLE_INSPECTOR,
+    /**
+     * Grants the user the ability to perform actions on other users and create
+     * flatboxes.
+     */
+    ROLE_MODERATOR,
+    /**
+     * Enables the user to create flatboxes.
+     */
+    ROLE_SUPER_EDITOR
 }
