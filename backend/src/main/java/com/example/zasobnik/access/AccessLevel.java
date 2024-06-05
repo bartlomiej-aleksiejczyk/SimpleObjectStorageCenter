a1b0b5aab3b0839eec63d84bf2e7676497465b0b
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-06-05 19:35:36 +0200

    Add docstring to flatbox-level  access roles

--- backend/src/main/java/com/example/zasobnik/access/AccessLevel.java
+++ backend/src/main/java/com/example/zasobnik/access/AccessLevel.java
@@ -1,5 +1,17 @@
 package com.example.zasobnik.access;
 
 public enum AccessLevel {
-    READER, EDITOR, OWNER
+    /**
+     * Allows the user to read content from a specified flatbox.
+     */
+    READER,
+    /**
+     * Enables the user to read and modify the content of a flatbox.
+     */
+    EDITOR,
+    /**
+     * Grants the user permissions to read and modify both the content and settings
+     * of a flatbox.
+     */
+    OWNER
 }
